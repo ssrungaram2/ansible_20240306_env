5238a90bedd44d18b2c4f685b4d674eab7ef60d8
“Seetaram <ssrungaram2@gmail.com>
2024-03-29 18:36:27 -0400

    added ansible

---
+++ ansible.sh
@@ -0,0 +1,4 @@
+component=$1
+env=$2
+ansible-playbook -i ${component}-${dev}.prajasevak.info, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense.yml -e role_name=${component} -e env=${env}
+
