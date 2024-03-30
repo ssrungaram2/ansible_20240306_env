b5301889634fe9ac2a6a79a6ca3121368c3c6851
“Seetaram <ssrungaram2@gmail.com>
2024-03-29 22:51:43 -0400

    added env

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,4 @@
 component=$1
-env
-#ansible-playbook -i ${component}-${env}.prajasevak.info, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense.yml -e role_name=${component} -e env=${env}
+#env
+ansible-playbook -i ${component}-${env}.prajasevak.info, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense.yml -e role_name=${component} -e env=${env}
 
